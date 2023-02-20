e09ee654d2146aeabda3502573fa194bbdc05531
KristinaMacias <maciaskristina@gmail.com>
2023-02-19 18:43:27 -0800

    text validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,13 +5,27 @@ import Button from './shared/Button'
 
 function FeedbackForm() {
     // component state
-    const [text, setText] = useState(''); 
+    const [text, setText] = useState(''); //for input text
+    const [btnDisabled, setBtnDisabled] = useState(true); //to validate input
+    const [message, setMessage] = useState(''); //message for validation
 
     //update text with user input from form
     const handleTextChange = (e) => { 
-        e.preventDefault();
-        setText(text);
+        if(text === '') {
+          setBtnDisabled(true); //if empty string, disable button
+          setMessage(null); //null b/c message shouldn't show until a user starts typing
+        } else if(text !== '' && text.trim().length <= 10) { //if not empty and less than or equal to 10 (trim will remove whitespace from count)
+          setBtnDisabled(true);
+          setMessage('Must include at least 10 characters');
+        } else {
+          setMessage(null);
+          setBtnDisabled(false);
+        }
+
+        setText(e.target.value);
+        console.log("length of text: ", text.length)
     }
+
 
   return (
     <Card>
@@ -19,10 +33,12 @@ function FeedbackForm() {
         <h2>How would you rate your experience?</h2>
         <div className="input-group">
           <input onChange={handleTextChange} value={text} type="text" placeholder="Share your experience" />
-          <Button>
+          <Button type='submit' version='primary' isDisabled={btnDisabled}>
             Send
           </Button>
         </div>
+        {message && <div className="message">{message}</div>}
+        
       </form>
     </Card>
   );
