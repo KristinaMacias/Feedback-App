cbedcbe9b4c5cbaefbd3307a0c98aa73da08ba27
KristinaMacias <maciaskristina@gmail.com>
2023-02-19 21:18:01 -0800

    fixed bug with form validation. Needed to destructure the value passed into the function

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,44 +1,49 @@
 import React from "react";
 import { useState } from "react";
 import Card from "./shared/Card";
-import Button from './shared/Button'
+import Button from "./shared/Button";
 
 function FeedbackForm() {
-    // component state
-    const [text, setText] = useState(''); //for input text
-    const [btnDisabled, setBtnDisabled] = useState(true); //to validate input
-    const [message, setMessage] = useState(''); //message for validation
+  // component state
+  const [text, setText] = useState(""); //for input text
+  const [btnDisabled, setBtnDisabled] = useState(true); //to validate input
+  const [message, setMessage] = useState(""); //message for validation
 
-    //update text with user input from form
-    const handleTextChange = (e) => { 
-        if(text === '') {
-          setBtnDisabled(true); //if empty string, disable button
-          setMessage(null); //null b/c message shouldn't show until a user starts typing
-        } else if(text !== '' && text.trim().length <= 10) { //if not empty and less than or equal to 10 (trim will remove whitespace from count)
-          setBtnDisabled(true);
-          setMessage('Must include at least 10 characters');
-        } else {
-          setMessage(null);
-          setBtnDisabled(false);
-        }
+  //update text with user input from form
+  //cant simply check text length since that waits until the next render
+  const handleTextChange = ({ target: { value } }) => { // get the value passed in. if its "", then disable button and no message
+    console.log("printing value from handleChange: ", value);
 
-        setText(e.target.value);
-        console.log("length of text: ", text.length)
+    if (value === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (value.trim().length < 10) { // if text is there, but less than 10, display message and disable button
+      setMessage('Text must be at least 10 characters');
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
     }
-
+    setText(value);
+  }
 
   return (
     <Card>
       <form>
         <h2>How would you rate your experience?</h2>
         <div className="input-group">
-          <input onChange={handleTextChange} value={text} type="text" placeholder="Share your experience" />
-          <Button type='submit' version='primary' isDisabled={btnDisabled}>
+          <input
+            onChange={handleTextChange}
+            value={text}
+            type="text"
+            placeholder="Share your experience"
+            minLength={10}
+          />
+          <Button type="submit" version="primary" isDisabled={btnDisabled}>
             Send
           </Button>
         </div>
         {message && <div className="message">{message}</div>}
-        
       </form>
     </Card>
   );
